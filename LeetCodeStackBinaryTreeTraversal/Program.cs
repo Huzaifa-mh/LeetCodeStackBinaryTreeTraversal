f5bd2f902cb72d710ffb41afef7bd5a3c88e9edb
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-11-01 12:43:24 +0500

    Update Program.cs

--- LeetCodeStackBinaryTreeTraversal/Program.cs
+++ LeetCodeStackBinaryTreeTraversal/Program.cs
@@ -2,6 +2,41 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+    }
+
+    //definition for a binary tree node.
+    public class treenode
+    {
+        public int val;
+        public treenode left;
+        public treenode right;
+        public treenode(int val = 0, treenode left = null, treenode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public IList<int> InorderTraversal(treenode root)
+    {
+
+        List<int> list = new List<int>();
+        inorder(root, list);
+
+        return list;
+    }
+    public void inorder(treenode node, List<int> list)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        inorder(node.left, list);
+        list.Add(node.val);
+        inorder(node.right, list);
+
     }
 }
